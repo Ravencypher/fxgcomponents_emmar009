232dd2c71edee15129171249d2b5a4224e477be7
Ravencypher <ravencypher@outlook.com>
2022-11-03 10:53:50 -0400

    Recréation de tout le dossier

---
+++ lib/stories/Pagination.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css'
+
+import { Pagination } from '../lib/components/Pagination/Pagination';
+
+export default{
+    title:'Components-TP1/Pagination',
+    component: Pagination
+};
+
+const Template = (args) => <Pagination {...args} />;
+
+export const Dix = Template.bind({});
+Dix.args={
+    defaultCurrent: 1,
+    total: 100
+};
+export const Vingt = Template.bind({});
+Vingt.args={
+    defaultCurrent: 1,
+    total: 200
+};
+export const Trente = Template.bind({});
+Trente.args={
+    defaultCurrent: 1,
+    total: 300
+};
